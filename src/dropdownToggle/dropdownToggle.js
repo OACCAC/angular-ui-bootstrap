46f7204d095c8fdb23a04b52a02f51178cdad03f
daryl.cogswell <daryl.cogswell@ccac-ont.ca>
2015-03-19 14:14:22 -0400

    Updated dropdownToggle to a hybrid of latest version (0.12.1) with append-to-body and close-on-click functionality from 0.7.

--- src/dropdownToggle/dropdownToggle.js
+++ src/dropdownToggle/dropdownToggle.js
@@ -1,101 +1,192 @@
-/*
- * dropdownToggle - Provides dropdown menu functionality in place of bootstrap js
- * @restrict class or attribute
- * @example:
-   <li class="dropdown">
-     <a class="dropdown-toggle">My Dropdown Menu</a>
-     <ul class="dropdown-menu">
-       <li ng-repeat="choice in dropChoices">
-         <a ng-href="{{choice.href}}">{{choice.text}}</a>
-       </li>
-     </ul>
-   </li>
- */
+angular.module('ui.bootstrap.dropdownToggle', ['ui.bootstrap.position'])
 
-angular.module('ui.bootstrap.dropdownToggle', []).directive('dropdownToggle', ['$document', '$location', '$position','$animate','$timeout', function ($document, $location, $position, $animate,$timeout) {
-  var openElement = null,
-    closeMenu   = angular.noop;
-  return {
-    restrict: 'CA',
-    link: function(scope, element, attrs) {
-      var dropdownMenu = element.parent().find('.dropdown-menu');
+    .constant('dropdownConfig', {
+      openClass: 'open'
+    })
 
-      var ddPosition = $position.position(dropdownMenu);
-      ddPosition.cssPosition = dropdownMenu.prop('position');
+    .service('dropdownService', ['$document', function($document) {
+      var openScope = null;
 
-      var appendToBody = angular.isDefined(attrs.appendToBody) ? scope.$eval(attrs.appendToBody) : false;
-      var closeOnClick = angular.isDefined(attrs.closeOnClick) ? scope.$eval(attrs.closeOnClick) : true;
-
-      function updateDropdownMenuPosition() {
-        btnPosition = $position.position(element);
-        btnOffset = $position.offset(element);
-        var newPosition = {
-          top: btnPosition.top + btnOffset.top + btnPosition.height,
-          left: btnPosition.left + btnOffset.left
-        };
-        dropdownMenu.css(newPosition);
-      }
-
-      if(appendToBody){
-        //Move element to the body
-        $animate.move(dropdownMenu, $document.find('body'));
-        dropdownMenu.css({position: 'absolute'});
-        updateDropdownMenuPosition();
-      }
-
-      scope.$watch('$location.path', function() { closeMenu(); });
-
-      if(closeOnClick){
-        element.parent().bind('click', function() { closeMenu(); });
-      }
-
-      element.bind('click', function (event) {
-
-        var elementWasOpen = (element === openElement);
-
-        if(closeOnClick){//Accomodates clickable items in the menu without closing it
-          event.preventDefault();
-          event.stopPropagation();
+      this.open = function( dropdownScope ) {
+        if ( !openScope ) {
+          $document.bind('click', closeDropdown);
+          $document.bind('keydown', escapeKeyBind);
         }
 
-        if (!!openElement) {
-          closeMenu();
+        if ( openScope && openScope !== dropdownScope ) {
+          openScope.isOpen = false;
         }
 
-        if (!elementWasOpen && !element.hasClass('disabled') && !element.prop('disabled')) {
+        openScope = dropdownScope;
+      };
 
-          if(appendToBody){
-            updateDropdownMenuPosition();
-            dropdownMenu.addClass('show');
+      this.close = function( dropdownScope ) {
+        if ( openScope === dropdownScope ) {
+          openScope = null;
+          $document.unbind('click', closeDropdown);
+          $document.unbind('keydown', escapeKeyBind);
+        }
+      };
+
+      var closeDropdown = function( evt ) {
+        // This method may still be called during the same mouse event that
+        // unbound this event handler. So check openScope before proceeding.
+        if (!openScope) { return; }
+
+        var toggleElement = openScope.getToggleElement();
+        if ( evt && toggleElement && toggleElement[0].contains(evt.target) ) {
+          return;
+        }
+
+        openScope.$apply(function() {
+          openScope.isOpen = false;
+        });
+      };
+
+      var escapeKeyBind = function( evt ) {
+        if ( evt.which === 27 ) {
+          openScope.focusToggleElement();
+          closeDropdown();
+        }
+      };
+    }])
+
+    .controller('DropdownController', ['$scope', '$attrs', '$parse', 'dropdownConfig', 'dropdownService', '$animate', function($scope, $attrs, $parse, dropdownConfig, dropdownService, $animate) {
+      var self = this,
+          scope = $scope.$new(), // create a child scope so we are not polluting original one
+          openClass = dropdownConfig.openClass,
+          getIsOpen,
+          setIsOpen = angular.noop,
+          toggleInvoker = $attrs.onToggle ? $parse($attrs.onToggle) : angular.noop;
+
+      this.init = function( element ) {
+        self.$element = element;
+
+        if ( $attrs.isOpen ) {
+          getIsOpen = $parse($attrs.isOpen);
+          setIsOpen = getIsOpen.assign;
+
+          $scope.$watch(getIsOpen, function(value) {
+            scope.isOpen = !!value;
+          });
+        }
+      };
+
+      this.toggle = function( open ) {
+        return scope.isOpen = arguments.length ? !!open : !scope.isOpen;
+      };
+
+      // Allow other directives to watch status
+      this.isOpen = function() {
+        return scope.isOpen;
+      };
+
+      scope.getToggleElement = function() {
+        return self.toggleElement;
+      };
+
+      scope.focusToggleElement = function() {
+        if ( self.toggleElement ) {
+          self.toggleElement[0].focus();
+        }
+      };
+
+      scope.$watch('isOpen', function( isOpen, wasOpen ) {
+        $animate[isOpen ? 'addClass' : 'removeClass'](self.$element, openClass);
+
+        if ( isOpen ) {
+          scope.focusToggleElement();
+          dropdownService.open( scope );
+        } else {
+          dropdownService.close( scope );
+        }
+
+        setIsOpen($scope, isOpen);
+        if (angular.isDefined(isOpen) && isOpen !== wasOpen) {
+          toggleInvoker($scope, { open: !!isOpen });
+        }
+      });
+
+      $scope.$on('$locationChangeSuccess', function() {
+        scope.isOpen = false;
+      });
+
+      $scope.$on('$destroy', function() {
+        scope.$destroy();
+      });
+    }])
+
+    .directive('dropdown', function() {
+      return {
+        controller: 'DropdownController',
+        link: function(scope, element, attrs, dropdownCtrl) {
+          dropdownCtrl.init( element );
+        }
+      };
+    })
+
+    .directive('dropdownToggle', ['$animate','$document','$position', function($animate,$document,$position) {
+      return {
+        require: '?^dropdown',
+        link: function(scope, element, attrs, dropdownCtrl) {
+          if ( !dropdownCtrl ) {
+            return;
           }
 
+          dropdownCtrl.toggleElement = element;
 
-          element.parent().addClass('open');
-          openElement = element;
-          closeMenu = function (event) {
+          var toggleDropdown = function(event) {
+            event.preventDefault();
 
-            if (event) {
-              if(!closeOnClick && (event.target == element[0] || angular.element(dropdownMenu).find(event.target).length)){
-                return; //Do not close the menu if the click originated inside the menu and closeOnClick is false.
-              }
-
-              event.preventDefault();
-              event.stopPropagation();
+            if ( !element.hasClass('disabled') && !attrs.disabled ) {
+              scope.$apply(function() {
+                dropdownCtrl.toggle();
+              });
             }
+          };
 
-            if(appendToBody){
-              //Hide the dropdown menu with the show class
+          var dropdownMenu = element.parent().find('.dropdown-menu');
+          var appendToBody = angular.isDefined(attrs.appendToBody) ? scope.$eval(attrs.appendToBody) : false;
+          var closeOnClick = angular.isDefined(attrs.closeOnClick) ? scope.$eval(attrs.closeOnClick) : true;
+          if(!closeOnClick){
+            dropdownMenu.bind('click', function(event){
+              event.stopPropagation();
+            });
+          }
+          function updateDropdownMenuPosition() {
+            btnPosition = $position.position(element);
+            btnOffset = $position.offset(element);
+            var newPosition = {
+              top: btnPosition.top + btnOffset.top + btnPosition.height,
+              left: btnPosition.left + btnOffset.left
+            };
+            dropdownMenu.css(newPosition);
+          }
+
+          if(appendToBody){
+            //Move element to the body
+            $animate.move(dropdownMenu, $document.find('body'));
+            dropdownMenu.css({position: 'absolute'});
+            updateDropdownMenuPosition();
+          }
+
+          element.bind('click', toggleDropdown);
+
+          // WAI-ARIA
+          element.attr({ 'aria-haspopup': true, 'aria-expanded': false });
+          scope.$watch(dropdownCtrl.isOpen, function( isOpen ) {
+            var open = !!isOpen;
+            if(open){
+              dropdownMenu.addClass('show');
+            } else {
               dropdownMenu.removeClass('show');
             }
 
-            $document.unbind('click', closeMenu);
-            element.parent().removeClass('open');
-            closeMenu = angular.noop;
-            openElement = null;
-          };
-          $document.bind('click', closeMenu);
+          });
+
+          scope.$on('$destroy', function() {
+            element.unbind('click', toggleDropdown);
+          });
         }
-      });
-    }
-  };
-}]);
+      };
+    }]);
